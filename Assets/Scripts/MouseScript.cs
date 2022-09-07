1ccbf590b43aca7d6e74fcf59938aa8b8c606191
kvcsmate <kvcsmate98@gmail.com>
2022-09-08 00:50:42 +0200

    Movement revamp
    oWo 😴

---
+++ Assets/Scripts/MouseScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class MouseScript : MonoBehaviour
+{
+    private Vector2 mousePos;
+    UnityEvent keydownEvent;
+
+    public UnityEvent KeyDownEvent 
+    {
+        get => keydownEvent; 
+        set => keydownEvent = value;
+    }
+
+    public Vector2 MousePos
+    {
+        get => mousePos;
+        set => mousePos = value;
+    }
+
+    public delegate void RightClickDown(Vector2 mPos);
+    public static event RightClickDown OnRightClickDown;
+
+    // event delegation : https://www.monkeykidgc.com/2020/07/how-to-use-events-in-unity-with-c.html
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KeyDownEvent = new UnityEvent();
+        KeyDownEvent.AddListener(OnKeyDown);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // #1 ezt kecire nem így kéne, majd megnézem hogy
+        // #2 jövőben nem beleégetni hanem szépen kiszedni változóba
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            KeyDownEvent.Invoke();
+            //ennek nagy része felesleges, de később jó lesz #copium
+        }
+    }
+
+    void OnKeyDown()
+    {
+        //Debug.Log("MousePos: "+MousePos.x+","+MousePos.y);
+        // ha nincs senki feliratkozva, exception-t dob.
+        if (OnRightClickDown != null)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                MousePos = hit.point;
+            }
+            OnRightClickDown(MousePos);
+        }
+    }
+}
--- Assets/Scripts/TestPlayerScript.cs
+++ Assets/Scripts/TestPlayerScript.cs
@@ -1,39 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+//always remove not used libraries
 
 public class TestPlayerScript : MonoBehaviour
 {
-    private Vector3 newPostion;
+    private Vector3 newPosition;
     private float newT;
     private Camera _camera;
 
-    public float speed = 0.04f;
+    private bool isFollowOn;
+
+    private Vector2 targetPos;
+
+    public Vector2 TargetPos
+    {
+        get => targetPos;
+        set => targetPos = value;
+    }
+
+    public Vector2 CurrPos
+    {
+        get => new Vector2(transform.position.x,transform.position.y); 
+    }
+
+
+    public float speed = 4f;
+
+    //mivel semmi sem használja egyelőre a dolgokat, nem muszáj, de getter setter-re szokjunk rá, ahol van értelme
 
     // Start is called before the first frame update
     void Start()
     {
-        newPostion = transform.position;
+        newPosition = transform.position;
         _camera = Camera.main;
         _camera.GetComponent<TestCameraFollow>().ObjectToFollow = gameObject;
         Debug.Log(_camera.gameObject.name);
+
+        MouseScript.OnRightClickDown += RefreshTargetPos;
+
+    }
+
+    void RefreshTargetPos(Vector2 mousePos)
+    {
+        Debug.Log("Másik oldal:" +mousePos);
+        targetPos = mousePos;
+    }
+
+    void MoveToTarget()
+    {
+        //Distance function returns the distance between two vector2's in float
+        if (Vector2.Distance(targetPos, CurrPos)>.5f)
+        {
+            transform.position = Vector2.MoveTowards(CurrPos, targetPos, speed);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            {
-                newPostion = hit.point;
-                newT = Time.time;
-            }
 
-            Debug.Log(hit.point);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _camera.GetComponent<TestCameraFollow>().ObjectToFollow = isFollowOn ? gameObject: null;
+            isFollowOn = !isFollowOn;
+            Debug.Log("toggle");
         }
 
-        transform.position = Vector3.Lerp(transform.position, newPostion, speed);
+        // Ctrl-K-D komment, Ctrl K-U uncomment kijelölt szöveg
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        //    {
+        //        newPostion = hit.point;
+        //        newT = Time.time;
+        //    }
+
+        //    Debug.Log(hit.point);
+        //}
+        //transform.position = Vector3.Lerp(transform.position, targetPos, speed);
+        MoveToTarget();
+
+    }
+
+    void OnEnable()
+    {
+        MouseScript.OnRightClickDown += RefreshTargetPos;
+    }
+
+    void OnDisable()
+    {
+        MouseScript.OnRightClickDown -= RefreshTargetPos;
     }
 }
