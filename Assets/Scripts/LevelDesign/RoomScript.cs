781af3f249b376b2bf5a838c4119f8804a11d523
kvcsmate <kvcsmate98@gmail.com>
2022-10-01 01:23:51 +0200

    created the idea
    needs programming

---
+++ Assets/Scripts/LevelDesign/RoomScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OpenSides 
+{
+    
+}
+
+public class RoomScript : MonoBehaviour
+{
+    public bool OpenToUp;
+
+    public bool OpenTodown;
+
+    public bool OpenToLeft;
+
+    public bool OpenToRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
